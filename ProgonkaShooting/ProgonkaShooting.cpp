fedd2c0be2f6851f0ef85474ad6171e52150a1d3
Danis222 <danisvil@mail.ru>
2022-10-26 00:19:36 +0300

    Обратный шаг

--- ProgonkaShooting/ProgonkaShooting.cpp
+++ ProgonkaShooting/ProgonkaShooting.cpp
@@ -33,8 +33,8 @@ double C(double x, double h) {
 
 int main()
 {
-    int f = 3, d1 = 3, d2 = -1, d = 4, e1 = 1, e2 = -2, e = 1, a = 0, b = 1;
-    const int n = 10;
+    double f = 3, d1 = 3, d2 = -1, d = 4, e1 = 1, e2 = -2, e = 1, a = 0, b = 1;
+    const int n = 1000;
     double h = (b - a) / n;
     double r1 = d2 / (d2 - d1 * h), r2 = e2 / (e1 * h + e2), s1 = d * h / (d1 * h - d2), s2 = e * h / (e1 * h + e2);
     if (r1 > 1 && r2 > 1) return 0;
@@ -44,14 +44,20 @@ int main()
             return 0;
         }
     }
-    double answer[n];
-    double alpha = r1, beta = s1;
-    answer[0] = (s2 + beta * r2) / (1 - alpha * r2);
+    double alphas[n], betas[n], y[n];
+    alphas[0] = r1; 
+    betas[0] = s1;
     for (int i = 1; i < n; i++) {
-        double x = a + (i - 1) * h;
-        beta = (beta * A(x, h) - f) / (B(x, h) - alpha * A(x, h));
-        alpha = C(x, h) / (B(x, h) - alpha * A(x, h));
-        answer[i] = (s2 + beta * r2) / (1 - alpha * r2);
-        std::cout << i << ": " << answer[i] << "\n";
+        double x = a + i * h;
+        alphas[i] = C(x, h) / (B(x, h) - alphas[i - 1] * A(x, h));
+        betas[i] = (betas[i - 1] * A(x, h) - f) / (B(x, h) - alphas[i - 1] * A(x, h));
+    }
+
+    y[n - 1] = (betas[n - 1] * r2 + s2) / (1 - alphas[n - 1] * r2);
+    for (int i = n - 2; i >= 0; i--) {
+        y[i] = alphas[i + 1] * y[i + 1] + betas[i + 1];
+    }
+    for (int i = 0; i < n; i++) {
+        std::cout << a + i * h << ": " << y[i] << "\n";
     }
 }
