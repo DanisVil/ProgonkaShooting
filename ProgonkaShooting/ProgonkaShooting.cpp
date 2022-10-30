e40c64ed3577b5033a69c2668e61fd544a8c46dd
Danis222 <danisvil@mail.ru>
2022-10-30 17:22:42 +0300

    метод стрельбы

--- ProgonkaShooting/ProgonkaShooting.cpp
+++ ProgonkaShooting/ProgonkaShooting.cpp
@@ -1,66 +1,44 @@
 ﻿#include <iostream>
 
-
 double p(double x) {
-    return -3 * (x + 1) * (x + 1);
+    return -7;
 }
-
 
 double q(double x) {
-    return -2 / (x + 1) / (x + 1);
+    return 12;
 }
 
+double f(double x) {
+    return 5;
+}
 
 double u(double x) {
-    return 1 / (x + 1);
+    return (1.0 / 4.0) * exp(4 * x) + (1.0 / 3.0) * exp(3 * x) + 5.0 / 12.0;
 }
-
-
-double A(double x, double h) {
-    return 1 / (h * h) - p(x) / (2 * h);
-}
-
-
-double B(double x, double h) {
-    return 2 / (h * h) - q(x);
-}
-
-
-double C(double x, double h) {
-    return 1 / (h * h) + p(x) / (2 * h);
-}
-
 
 int main()
 {
-    double f = 3, d1 = 3, d2 = -1, d = 4, e1 = 1, e2 = -2, e = 1, a = 0, b = 1;
-    const int n = 1000;
-    double h = (b - a) / n;
-    double r1 = d2 / (d2 - d1 * h), r2 = e2 / (e1 * h + e2), s1 = d * h / (d1 * h - d2), s2 = e * h / (e1 * h + e2);
-    if (r1 > 1 && r2 > 1) return 0;
-    for (int i = 1; i < n; i++) {
+    const int n = 100;
+    double a = 0, b = 1, A = 1, C = 2, h = (b-a)/n;
+    double D0 = A + h, D1 = h;
+    double y[2][n + 1];
+    y[0][0] = A;
+    y[0][1] = D0;
+    y[1][0] = 0;
+    y[1][1] = D1;
+    for (int i = 2; i <= n; i++) {
         double x = a + i * h;
-        if (abs(B(x, h)) < abs(A(x, h)) + abs(C(x, h))) {
-            return 0;
-        }
+        y[0][i] = (h * h * f(x) - (1.0 - (h / 2) * p(x)) * y[0][i - 2] - (h * h * q(x) - 2) * y[0][i - 1]) / (1 + h / 2 * p(x));
+        y[1][i] = (-(1 - h / 2 * p(x)) * y[1][i - 2] - (h * h * q(x) - 2) * y[1][i - 1]) / (1 + h / 2 * p(x));
     }
-    double alphas[n], betas[n], y[n + 1];
-    alphas[0] = r1;
-    betas[0] = s1;
-    for (int i = 1; i < n; i++) {
-        double x = a + i * h;
-        alphas[i] = C(x, h) / (B(x, h) - alphas[i - 1] * A(x, h));
-        betas[i] = (betas[i - 1] * A(x, h) - f) / (B(x, h) - alphas[i - 1] * A(x, h));
-    }
-
-    y[n] = (betas[n - 1] * r2 + s2) / (1 - alphas[n - 1] * r2);
-    double maxErr = abs(y[n] - u(b));
-    for (int i = n - 1; i >= 0; i--) {
-        y[i] = alphas[i] * y[i + 1] + betas[i];
-        if (abs(y[i] - u(a + i * h) > maxErr)) maxErr = abs(y[i] - u(a + i * h));
-    }
+    double B = u(b);
+    double c1 = (B - (double)y[0][n]) / y[1][n];
+    double maxErr = 0;
     for (int i = 0; i <= n; i++) {
-        std::cout << a + i * h << ": " << y[i] << "\n";
+        double x = a + i * h;
+        double Y = y[0][i] + c1 * y[1][i];
+        if (abs(u(x) - Y) > maxErr) maxErr = abs(u(x) - Y);
+        std::cout << x << ": " << Y << "\n";
     }
-    std::cout << "Maximum error: " << maxErr;
+    std::cout << "maximum error: " << maxErr;
 }
